4d4b546cb3224e7facf4d974f9e909bcd97d5ae1
fdodino <fernando.dodino@gmail.com>
2017-05-11 21:06:19 -0300

    Using arrow functions for specs

--- spec/clientes.js
+++ spec/clientes.js
@@ -1,9 +1,9 @@
-describe('clientes', function() {
+describe('clientes', () => {
   let manuel
   let manuelSpecial
   let manuelDoubty
 
-  beforeEach(function () {
+  beforeEach(() => {
     manuel = new Cliente()
     manuelDoubty = new Cliente()
     manuelDoubty.dudoso()
@@ -17,16 +17,16 @@ describe('clientes', function() {
     manuelSpecial.deuda = 1000
   })
 
-  it('monto maximo es el 30% de los ingresos', function() {
+  it('monto maximo es el 30% de los ingresos', () => {
   	expect(3000).toBe(manuel.montoMaximoCredito())
   }) 
-  it('monto maximo de un especial es el 30% de los ingresos + 2000', function() {
+  it('monto maximo de un especial es el 30% de los ingresos + 2000', () => {
     expect(5000).toBe(manuelSpecial.montoMaximoCredito())
   })
-  it('monto maximo de un dudoso poco deudor es 100', function() {
+  it('monto maximo de un dudoso poco deudor es 100', () => {
     expect(100).toBe(manuelDoubty.montoMaximoCredito())
   })
-  it('monto maximo de un dudoso muy deudor es 30% de los ingresos', function() {
+  it('monto maximo de un dudoso muy deudor es 30% de los ingresos', () => {
     expect(1500).toBe(manuelPocoDeudorDudoso.montoMaximoCredito())
   })
 })
